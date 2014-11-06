8d94727af284b13680ee7f37c9e047216798eec4
Brian <iamamthwiz+GitHub@gmail.com>
2014-11-05 21:00:10 -0800

    add ssh key check

--- oh-my-fish/custom/plugins/aa/aa_update.fish
+++ oh-my-fish/custom/plugins/aa/aa_update.fish
@@ -1,5 +1,21 @@
 set repos prep-work/ meta/ assessment-prep/ solutions/ ruby-curriculum/ sql-curriculum/ rails-curriculum/ js-curriculum/ lightning-talks/ job-search-curriculum/ jobberwocky/ algorithms-curriculum/ backbone-curriculum/
 
+function ssh_check
+    if ssh-add -l | grep -oq 'bitbucket'
+        echo 'Bitbucket key already added'
+    else
+        ssh-BB
+        echo 'Bitbucket key has been added'
+    end
+
+    if ssh-add -l | grep -oq 'github'
+        echo 'GitHub key already added'
+    else
+        ssh-GH
+        echo 'GitHub key has been added'
+    end
+end
+
 function aa_pull
     for repo in $repos
         echo $repo pull
@@ -17,6 +33,7 @@ function aa_push
 end
 
 function aa_update
+    ssh_check
     aa_pull
     aa_push
 end
