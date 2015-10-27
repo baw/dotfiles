5e6d48967a7edc9902b56af4c9c4a5e00e1ffe50
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-10-27 01:55:49 -0700

    add 'c' command for 'clear'

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,11 +7,27 @@ set fish_theme robbyrussell
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins rvm up git mkcd activator jvm aa ssh-add haskell
+set fish_plugins rvm up git general activator jvm aa ssh-add haskell
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
-set PATH /Applications/Postgres.app/Contents/Versions/9.3/bin $PATH
+set PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+# add thefuck
+function fuck -d 'Correct your previous console command'
+    set -l exit_code $status
+    set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
+    set -l fucked_up_commandd $history[1]
+    thefuck $fucked_up_commandd > $eval_script
+    . $eval_script
+    rm $eval_script
+    if test $exit_code -ne 0
+        history --delete $fucked_up_commandd
+    end
+end
+
+# add android sdk to path
+set -x ANDROID_HOME ~/android-sdk-macosx
